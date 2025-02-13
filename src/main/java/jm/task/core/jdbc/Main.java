e07df849e3bb4200f269134f7b8704507a273b37
achach <achachoriginal2501@gmail.com>
2025-02-13 19:51:44 +0300

    add toString to user, create algorim of task in main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import java.util.List;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser( "Donatello", "Betto", ( byte ) 79 );
+        userService.saveUser( "Michelangelo", "Buonarroti", ( byte ) 88);
+        userService.saveUser( "Leonardo", "da Vinci", ( byte ) 67);
+        userService.saveUser( "Raffaello", "da Urbino", ( byte ) 37 );
+
+        List<User> userList = userService.getAllUsers();
+        userList.stream().forEach( u -> System.out.println( u ) );
+
+        userService.createUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -59,4 +59,14 @@ public class User {
     public void setAge(Byte age) {
         this.age = age;
     }
+    
+    @Override
+    public String toString() {
+        String result = String.format( "user's info: name: %s, lastname: %s, age: %s",
+            getName(),
+            getLastName(),
+            getAge()
+        );
+        return result;
+    }
 }
